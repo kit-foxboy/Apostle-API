3eb34380ffa6c128558144c26a12bec16036450a
Stephen Bissett <steve.bissett5@gmail.com>
2019-01-29 15:38:37 -0700

    initialschemaseed

---
+++ models/Schema.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE dsp;
+
+USE dsp;
+
+CREATE TABLE `employee` (
+  `idEmployee` int(11) NOT NULL,
+  `lastName` varchar(45) DEFAULT NULL,
+  `firstName` varchar(45) DEFAULT NULL,
+  `employeeNbr` varchar(45) NOT NULL,
+  `department` varchar(45) DEFAULT NULL,
+  `title` varchar(45) DEFAULT NULL,
+  `cellPhone` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idEmployee`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='employee table'
+
+CREATE TABLE `vehicle` (
+  `idVehicle` int(11) NOT NULL COMMENT 'vehcile primary key',
+  `vinNbr` varchar(45) NOT NULL,
+  `registrationNbr` varchar(45) DEFAULT NULL,
+  `registrationDate` varchar(45) DEFAULT NULL,
+  `make` varchar(45) DEFAULT NULL,
+  `model` varchar(45) DEFAULT NULL,
+  `year` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='vehicle table				'
---
+++ models/seeds.sql
@@ -0,0 +1,20 @@
+USE dsp;
+
+INSERT INTO employee (lastname,firstName,employeeNbr,department,title,cellPhone)
+VALUES 
+('Rigor', 'Stref','Bissett','Hanson'),
+('Chris', 'Alan',"Stephen",'Geoff'),
+('1','2','3','4'),
+('Shipping','Shipping','Shipping',"Shipping"),
+('lead','lead','lead','lead'),
+('555-555-333','333-333-3333','222-222-2222','666-666-6666');
+
+
+INSERT INTO vehicle (vinNbr, registrationNbr, registrationDate, make, model, year)
+VALUES 
+('12345','54432','455678','98765'),
+('YM12345','YM 54322','YU77777','YT00000'),
+('12/15/2018','11/15/2018','10/15/2016','12/17/2015'),
+('Dodge','Dodge','Dodge','Dodege'),
+('Powerwagon','Powerwagon','Powerwagon','Powerwagon'),
+('2018','2018','2015','2012');
