22d0c17a995bb55bbd3df88d740489bc9a0a1bd5
Geoff Lambert <admin@vulpineinteractive.com>
2019-02-14 14:41:54 -0700

    fixed inspections and updated models

--- models/employee.js
+++ models/employee.js
@@ -1,16 +1,19 @@
-module.exports = function (sequelize, DataTypes) {
-
-  var Employee = sequelize.define("Employee", {
-    lastName: DataTypes.STRING,
-    firstName: DataTypes.STRING,
-    employeeNbr: DataTypes.STRING,
-    department: DataTypes.STRING,
-    title: DataTypes.STRING,
-    cellPhone: DataTypes.STRING
-
-  },{
-    timestamps: false
-  });
+module.exports = function(sequelize, DataTypes) {
+  var Employee = sequelize.define(
+    "Employee",
+    {
+      userID: DataTypes.INTEGER,
+      lastName: DataTypes.STRING,
+      firstName: DataTypes.STRING,
+      employeeNbr: DataTypes.STRING,
+      department: DataTypes.STRING,
+      title: DataTypes.STRING,
+      cellPhone: DataTypes.STRING
+    },
+    {
+      timestamps: false
+    }
+  );
 
   return Employee;
 };
